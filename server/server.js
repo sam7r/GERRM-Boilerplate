d1a1cdb6db26a5f9c5a23e947f0d65ea5d6f9567
Samuel Roberton <samuel.j.roberton@accenture.com>
2016-03-03 18:17:57 +0000

    Moved mongoTestSetup to seed/seed.js (WIP) and added npm script to call seed action. Modified server.js moving functions into dev scope and adding notes

--- server/server.js
+++ server/server.js
@@ -3,54 +3,55 @@ import httpProxy from 'http-proxy';
 import path from 'path';
 import bundle from './bundle';
 import { MongoClient } from 'mongodb';
-import mongoTestSetup from './mongoTestSetup';
 
 const config = {
   publicFolder: path.join(__dirname, '..', 'public'),
-  // If accessing through a Docker container use ACCESS URL
-  mongoUrl: 'mongodb://192.168.99.100:32770',
-  mongoCollection: 'test' // Collection name to create for test data
+  /*
+    If accessing locally use Docker container ACCESS URL
+    If accessing from another container user port 27017
+  */
+  mongoUrl: 'mongodb://192.168.99.100:32771',
+  mongoCollection: 'test'
 };
 
 let db;
-const app = express();
+let app = express();
+let proxy = httpProxy.createProxyServer({ changeOrigin: true });
+
 const isProduction = process.env.NODE_ENV === 'production';
 const port = isProduction ? process.env.PORT : 3000;
 
 app.use(express.static(config.publicFolder));
 
-if (!isProduction) {
-  mongoTestSetup(config.mongoUrl, config.mongoCollection);
-}
-
 MongoClient.connect(`${config.mongoUrl}`, (err, database) => {
   if (err) throw err;
-  db = database;
+  db = database; // Connect to database
 });
 
 if (!isProduction) {
-
-  let proxy = httpProxy.createProxyServer({ changeOrigin: true });
+  /*
+    Using proxy we are able to use the files served from Webpack
+    enabling Hot Reload for faster development
+  */
   bundle();
   app.all('/assets/*', function (req, res) {
     proxy.web(req, res, {
-        target: 'http://localhost:8080'
+      // Virtual assets folder mapped to webpack dev server
+      target: 'http://localhost:8080'
     });
   });
-
-  app.get('/data/test', (req, res) => {
-    db.collection(config.mongoCollection).find({}).toArray((err, test) => {
-      if(err) throw err;
-      console.log(test);
-      res.json(test);
-    });
-  });
-
-  proxy.on('error', function(e) {
-    console.log('Could not connect to proxy, please try again...');
-  });
-
 }
+
+app.get('/data/test', (req, res) => {
+  db.collection(config.mongoCollection).find({}).toArray((err, test) => {
+    if(err) throw err;
+    res.json(test);
+  });
+});
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
 
 app.listen(port, function () {
   console.log('Server running on port ' + port);
